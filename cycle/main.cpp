0a3aa2b49c341bf0befa96aec5583088ff31d091
geTiger <36882747+geTiger@users.noreply.github.com>
2018-05-16 14:21:06 +0800

    mark
    mark

--- cycle/main.cpp
+++ cycle/main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 void Init()
 {
 	glClearColor(0.0, 0.0, 0.0, 0);
-	glColor3f(1.0, 0.0, 0.0);
+	glColor3f(2.0, 1.0, 0.0);
 	gluOrtho2D(0, 640, 480, 0);
 }
 
@@ -16,12 +16,13 @@ void display()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
 	ddacircle(200,200,100);
-	bresenhamcircle(400,200,100);
+	bresenhamcircle(-50,200,100);
 	glFlush();
 }
 
 int main(int argc, char **argv)
 {
+    //mark test
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	glutInitWindowPosition(100, 100);
